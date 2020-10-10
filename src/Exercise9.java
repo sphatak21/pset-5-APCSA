fb1c114d9231660592bed5d962dbddac24fe0a61
Sidd Phatak <sphatak@ucvts.org>
2020-10-10 19:11:17 -0400

    final commit

--- src/Exercise9.java
+++ src/Exercise9.java
@@ -5,35 +5,38 @@ public class Exercise9 {
         System.out.print("Enter a month: ");
         boolean month_found = false;
         String input = in.nextLine();
+        input = input.toUpperCase();
         String days = "";
-        String[] january = new String [] {"JANUARY", "JAN", "JANU", "31"};
-        String[] february = new String [] {"FEBRUARY", "FEB", "FEBR", "28", "29"};
-        String[] march = new String [] {"MARCH", "MAR", "MARC", "31"};
-        String[] april = new String [] {"APRIL", "APR", "APRI", "30"};
-        String[] may = new String [] {"MAY", "MAY", "MAY", "31"};
-        String[] june = new String [] {"JUNE", "JUN", "JUNE", "30"};
-        String[] july = new String [] {"JULY", "JUL", "JULY", "31"};
-        String[] august = new String [] {"AUGUST", "AUG", "AUGU", "31"};
-        String[] september = new String [] {"SEPTEMBER", "SEP", "SEPT", "31"};
-        String[] october = new String [] {"OCTOBER", "OCT", "OCTO", "31"};
-        String[] november = new String [] {"NOVEMBER", "NOV", "NOVE", "30"};
-        String[] december = new String [] {"DECEMBER", "DEC", "DECE", "31"};
+        String[] january = new String [] {"JANUARY", "31"};
+        String[] february = new String [] {"FEBRUARY", "28", "29"};
+        String[] march = new String [] {"MARCH", "31"};
+        String[] april = new String [] {"APRIL", "30"};
+        String[] may = new String [] {"MAY ", "31"};
+        String[] june = new String [] {"JUNE", "30"};
+        String[] july = new String [] {"JULY", "31"};
+        String[] august = new String [] {"AUGUST", "31"};
+        String[] september = new String [] {"SEPTEMBER", "31"};
+        String[] october = new String [] {"OCTOBER", "31"};
+        String[] november = new String [] {"NOVEMBER", "30"};
+        String[] december = new String [] {"DECEMBER", "31"};
         String [][] months = {january, february, march, april, may, june, july, august, september, october, november, december};
-
         for (int i = 0; i<months.length; i++){
-            for(int j = 0; j < months[i].length; j++){
                 String [] current_month = months[i];
-                if(input.toUpperCase().equals(current_month[j])){
+                String current_month_name = current_month[0];
+                if(input.equals(current_month_name) ||
+                        input.equals(current_month_name.substring(0, 3)) ||
+                        input.equals(current_month_name.substring(0,4))){
+
                     month_found = true;
                     if(current_month.length == 5){
                         days = current_month[current_month.length-2] + current_month[current_month.length-1];
                     }else {
                         days = current_month[current_month.length - 1];
                     }
+                    break;
                 }else{
                     month_found = false;
                 }
-            }
         }
         if(month_found == false){
             System.out.println("\nThat's not a valid month.");
@@ -48,5 +51,4 @@ public class Exercise9 {
         }
         in.close();
     }
-
 }
